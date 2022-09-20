605e925dffab703fab5d5770239a23a2bf1aa128
wfuertes@gmail.com <willian@refatorando.dev>
2022-09-19 21:42:53 -0600

    Remove turma 7

--- turma7/src/Item.ts
+++
@@ -1,5 +0,0 @@
-export default class Item {
-    constructor(readonly id: string, readonly name: string, readonly price: number) {
-        if (price < 0) throw new Error('Price must be greather than zero');
-    }
-}
